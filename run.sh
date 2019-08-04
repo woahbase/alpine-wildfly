c627f596bf60c0b39acf13e31383fd11a0a44e24
lapnapra <34398784+lapnapra@users.noreply.github.com>
2019-08-04 12:13:20 +0530

    added support for armv7l and aarch64

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 cd ${WILDFLY_HOME} || exit 1;
 chown -R ${PUID}:${PGID} /opt/jboss/;
 
+USERNAME="${USERNAME:-admin}"
 PASSWORD="${PASSWORD:-insecurebydefault}";
-s6-setuidgid ${PUID}:${PGID} ${WILDFLY_HOME}/bin/add-user.sh admin "${PASSWORD}" --silent
+s6-setuidgid ${PUID}:${PGID} ${WILDFLY_HOME}/bin/add-user.sh "${USERNAME}" "${PASSWORD}" --silent
 
 s6-setuidgid ${PUID}:${PGID} ${WILDFLY_HOME}/bin/standalone.sh  --server-config=${SERVERCONFIG} -b=0.0.0.0 -bmanagement=0.0.0.0
 
